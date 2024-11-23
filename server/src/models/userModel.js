7bef6c8a23f2a94f02d6ea816077f7a7626c0a22
Hari Maulana <harimaulana.xyz@gmail.com>
2024-11-24 03:13:26 +0700

    chore(server): add user model

---
+++ server/src/models/userModel.js
@@ -0,0 +1,27 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database.js";
+
+export const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
